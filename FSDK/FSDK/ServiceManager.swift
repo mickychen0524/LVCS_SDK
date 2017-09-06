5c6fbf3cfa32f29f648799ad40fa4f15cb3dc2f3
Swayam Agrawal <agrawal.swayam@gmail.com>
2017-09-06 17:35:06 +0530

    Global extension for Dictionary

--- FSDK/FSDK/ServiceManager.swift
+++ FSDK/FSDK/ServiceManager.swift
@@ -249,3 +249,11 @@ extension ServiceManager : MCSessionDelegate {
     }
     
 }
+
+public extension Dictionary {
+    subscript(i:Int) -> (key:Key,value:Value) {
+        get {
+            return self[index(startIndex, offsetBy: i)];
+        }
+    }
+}
